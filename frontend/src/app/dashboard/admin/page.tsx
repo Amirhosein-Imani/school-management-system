32b3f9b6bb171a2c59766e2a654f8753335a22d4
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2025-01-28 00:15:44 +0330

    Start admin page & Work on UserCard component

--- frontend/src/app/dashboard/admin/page.tsx
+++ frontend/src/app/dashboard/admin/page.tsx
@@ -1,8 +1,36 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
 
   return (
 
-    <div>AdminPage</div>
+    <div className="flex flex-col md:flex-row p-4 gap-4">
+
+      {/* LEFT */}
+
+      <div className="w-full lg:w-2/3">
+
+      {/* USER CARDS */}
+
+      <div className="flex flex-wrap justify-between gap-4">
+
+        <UserCard type="student" />
+
+        <UserCard type="parent" />
+
+        <UserCard type="teacher" />
+
+        <UserCard type="staff" />
+
+      </div>
+      
+      </div>
+
+      {/* RIGHT */}
+
+      <div className="w-full lg:1/3">RIGHT</div>
+
+    </div>
 
   )
 
