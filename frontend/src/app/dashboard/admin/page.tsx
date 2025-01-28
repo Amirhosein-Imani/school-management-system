78dc58b0d74aab2c0ee73cfdcf02a779705eb1c6
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2025-01-28 13:44:00 +0330

    Work on CountChart component for admin page

--- frontend/src/app/dashboard/admin/page.tsx
+++ frontend/src/app/dashboard/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
@@ -8,21 +9,43 @@ const AdminPage = () => {
 
       {/* LEFT */}
 
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
 
-      {/* USER CARDS */}
+        {/* USER CARDS */}
 
-      <div className="flex flex-wrap justify-between gap-4">
+        <div className="flex flex-wrap justify-between gap-4">
 
-        <UserCard type="student" />
+          <UserCard type="student" />
 
-        <UserCard type="parent" />
+          <UserCard type="parent" />
 
-        <UserCard type="teacher" />
+          <UserCard type="teacher" />
 
-        <UserCard type="staff" />
+          <UserCard type="staff" />
 
-      </div>
+        </div>
+
+        {/* MIDDLE CHARTS */}
+
+        <div className="flex flex-col lg:flex-row gap-4">
+
+          {/* COUNT CHART */}
+
+          <div className="w-full lg:1/3 h-[450px]">
+          
+            <CountChart />
+
+          </div>
+
+          {/* ATTENDANCE CART */}
+
+          <div className="w-full lg:2/3 h-[450px]"></div>
+
+        </div>
+
+        {/* BOTTOM CHART */}
+
+        <div className=""></div>
       
       </div>
 
